ae7bc3c6eefea4776672ce30a264ed46379d3d3c
ShinoaShiro <edwin.indarto95@gmail.com>
2023-04-18 08:18:07 +0800

    Commit Model

--- model.py
+++ model.py
@@ -1,33 +1,49 @@
-from keras.models import Sequential
-from keras.layers.convolutional import Convolution2D
-from keras.layers import Flatten, Dense, Lambda, Convolution2D, Cropping2D, Dropout, Reshape, BatchNormalization, ELU
-from keras.optimizers import Adam
 
+from keras.models import Sequential
+from keras.layers.convolutional import Conv2D
+from keras.layers import Flatten, Dense, Lambda, Conv2D, Cropping2D, Dropout, Reshape, BatchNormalization, ELU, ReLU
+from tensorflow.keras.optimizers import Adam
+from keras import backend as K
+
+
+def rmse(y_pred, y_true):
+    return K.sqrt(K.mean(K.square(y_pred - y_true)))
 
 def CNNModel():
     model = Sequential()
-    model.add(Convolution2D(24, 5, 5, input_shape = (240, 320, 3), subsample=(2,2), init = 'he_normal'))
+    # model.add(Conv2D(24, kernel_size =(5, 5), input_shape = (360, 640, 3), strides=(2,2), kernel_initializer = 'he_normal'))
+    model.add(Conv2D(24, kernel_size =(5, 5), input_shape = (240, 320, 3), strides=(2,2), kernel_initializer = 'he_normal'))
+    # model.add(ReLU())
     model.add(ELU())
-    model.add(Convolution2D(36, 5, 5, subsample=(2,2), init = 'he_normal'))
+    model.add(Conv2D(36, kernel_size =(5, 5), strides=(2,2), kernel_initializer = 'he_normal'))
+    # model.add(ReLU())
     model.add(ELU())
-    model.add(Convolution2D(48, 5, 5, subsample=(2,2), init = 'he_normal'))
+    model.add(Conv2D(48, kernel_size =(5, 5), strides=(2,2), kernel_initializer = 'he_normal'))
+    # model.add(ReLU())
     model.add(ELU())
     model.add(Dropout(0.5))
-    model.add(Convolution2D(64, 3, 3, subsample = (1,1), init = 'he_normal'))
+    model.add(Conv2D(64, kernel_size =(3, 3), strides = (1,1), kernel_initializer = 'he_normal'))
+    # model.add(ReLU())
     model.add(ELU())
-    model.add(Convolution2D(64, 3, 3, subsample= (1,1), border_mode = 'valid', init = 'he_normal'))
+    model.add(Conv2D(64, kernel_size =(3, 3), strides= (1,1), padding = 'valid', kernel_initializer = 'he_normal'))
     model.add(Flatten())
+    # model.add(ReLU())
     model.add(ELU())
-    model.add(Dense(100, init = 'he_normal'))
+    model.add(Dense(100, kernel_initializer = 'he_normal'))
+    # model.add(ReLU())
     model.add(ELU())
-    model.add(Dense(50, init = 'he_normal'))
+    model.add(Dense(50, kernel_initializer = 'he_normal'))
     # model.add(Dropout(0.5))
+    # model.add(ReLU())
     model.add(ELU())
-    model.add(Dense(10, init = 'he_normal'))
+    model.add(Dense(10, kernel_initializer = 'he_normal'))
+    # model.add(ReLU())
     model.add(ELU())
-    model.add(Dense(1, init = 'he_normal'))
+    model.add(Dense(1, kernel_initializer = 'he_normal'))
 
-    adam = Adam(lr=1e-4)
-    model.compile(optimizer = adam, loss = 'mse')
+    adam = Adam(lr=1e-41)
+    # model.compile(optimizer = adam, loss = 'mse')
+    # model.compile(optimizer = adam, loss = ['mse', 'mae'], metrics=['mse', 'mae'])
+    model.compile(optimizer = adam, loss = ['mae', rmse], metrics=[rmse, 'mae'])
 
     return model
